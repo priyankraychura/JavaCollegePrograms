d2cc55c367e5f2485ba6f061d4c7625a61f46cbf
priyankraychura <priyankrkaychura@gmail.com>
2024-03-04 13:02:47 +0530

    Choice example completed

--- src/PR61_ChoiceExample.java
+++ src/PR61_ChoiceExample.java
@@ -3,11 +3,23 @@ import java.awt.*;
 
 public class PR61_ChoiceExample {
     public PR61_ChoiceExample() {
-        Frame frame = new Frame("Choice Example.");
+        JFrame frame = new JFrame("Choice Example.");
         Choice c = new Choice();
+
+        c.setBounds(50, 100, 75, 75);
+        c.add("Item1");
+        c.add("Item2");
+        c.add("Item3");
+        c.add("Item4");
+
+        frame.add(c);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        PR61_ChoiceExample js = new PR61_ChoiceExample();
     }
 }
