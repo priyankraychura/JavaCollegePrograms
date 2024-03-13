bbd948214d08f5efccaceda297fd08bfba4f5cc7
priyankraychura <priyankrkaychura@gmail.com>
2024-03-14 00:16:46 +0530

    JList example added

---
+++ src/PR72_SimpleList.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public interface PR72_SimpleList {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("List Example");
+        String[] fruits = {"Apple", "Banana", "Orange", "Grapes"};
+        JList list = new JList<>(fruits);
+
+        list.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                String data = (String) list.getSelectedValue();
+                System.out.println(data);
+            }
+        });
+
+        frame.add(list);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
