cb2ecdfac2cb518009398c40a1e6834743c9ce77
priyankraychura <priyankrkaychura@gmail.com>
2024-03-23 18:56:44 +0530

    Zooming Panel programe added

---
+++ src/PR75_ZoomingPanel.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+public class PR75_ZoomingPanel extends JPanel implements MouseWheelListener {
+    private double zoomFactor = 1.0;
+
+    public PR75_ZoomingPanel(){
+        super();
+        setBackground(Color.WHITE);
+        addMouseWheelListener(this);
+    }
+
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.scale(zoomFactor, zoomFactor);
+        g2d.drawString("Zoom in or zoom out using mouse wheel!", 50, 100);
+    }
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        int rotation = e.getWheelRotation();
+        zoomFactor += rotation * 0.1;
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Zooming panel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        PR75_ZoomingPanel panel = new PR75_ZoomingPanel();
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
